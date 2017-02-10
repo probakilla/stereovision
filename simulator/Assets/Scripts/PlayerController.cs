2f44e73b293cd785a58f344f83a2ccb8a90f7582
fr0uty <candela.francois@gmail.com>
2017-02-10 10:42:18 +0100

    simulation controller created

--- simulator/Assets/Scripts/PlayerController.cs
+++ simulator/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
 public class PlayerController : MonoBehaviour
 {
     public float m_Speed = 2f;
     public float m_TurnSpeed = 180f;
+    bool simulating = true;
 
     private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
     private string m_TurnAxisName;              // The name of the input axis for turning.
@@ -31,12 +37,73 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
-        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
     }
 
     // Update is called once per frame
-    void FixedUpdate () {
+    void FixedUpdate ()
+    {
+
+        const string input = "D:/command.txt";
+        const string output = "D:/image.txt";
+
+        //Starts a new reading cycle
+        bool opened = false;
+        while (!opened && simulating)
+        {
+            try
+            {
+                //Try to get the input from the controller
+                StreamReader sr = new StreamReader(input);
+                string command = sr.ReadLine();
+                string data = sr.ReadToEnd();
+                sr.Close();
+                File.Delete(input);
+
+                opened = true;      //Stop the reading cycle
+
+                //Reaction the the command of the controller
+                if (command == "stop")//Stop simulation
+                    simulating = false;
+                else
+                {
+                    if (command == "forward")
+                    {
+                        m_MovementInputValue = 0.5f;
+                        m_TurnInputValue = 0f;
+                    }
+                    else if(command == "backward")
+                    {
+                        m_MovementInputValue = -0.5f;
+                        m_TurnInputValue = 0f;
+                    }
+                    else if (command == "right")
+                    {
+                        m_TurnInputValue = 0.1f;
+                        m_MovementInputValue = 0f;
+                    }
+
+                    else if (command == "left")
+                    {
+                        m_TurnInputValue = -0.1f;
+                        m_MovementInputValue = 0f;
+                    }
+
+                    StreamWriter sw = new StreamWriter(output);
+                    sw.Write("hello");
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        if (!simulating)
+        {
+            m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+            m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+        }
         Move();
         Turn();
     }
@@ -44,7 +111,11 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        Vector3 movement;
+        if ( simulating)
+            movement = transform.forward * m_MovementInputValue * m_Speed;
+        else
+            movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
 
         // Apply this movement to the rigidbody's position.
         m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
@@ -54,7 +125,11 @@ public class PlayerController : MonoBehaviour
     private void Turn()
     {
         // Determine the number of degrees to be turned based on the input, speed and time between frames.
-        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        float turn;
+        if (simulating)
+            turn = m_TurnInputValue * m_TurnSpeed;
+        else
+            turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
 
         // Make this into a rotation in the y axis.
         Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
