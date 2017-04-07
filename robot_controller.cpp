72b7811dabddde7319dd343ad88978bfc8eba16e
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-04-07 16:17:27 +0200

    Code du controller

--- robot_controller.cpp
+++ robot_controller.cpp
@@ -1,13 +1,31 @@
 #include "robot_controller.h"
 
+robot_controller::robot_controller () : _last_dist (-1.0) {}
 
-double calc_dist (cv::Mat const & depth_map)
+float robot_controller::calc_dist (const cv::Mat & left_image, const cv::Mat & right_image)
 {
     // Todo.
     return 0;
 }
 
-void process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega)
+void robot_controller::process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega)
 {
 // Todo.
+    float dist (calc_dist(left_img, right_img));     // Distance from the object.
+    // Case of the first call.
+    if (_last_dist == -1)
+    {
+        *vx = INITIAL_SPEED;
+        _last_speed = INITIAL_SPEED;
+        _last_dist = dist;
+    }
+    else
+    {
+        float delta_d (dist - _last_dist);
+
+        float delta_v (delta_d / DT);
+        *vx = _last_speed + delta_v;
+        _last_speed = *vx;
+        _last_dist = dist;
+    }
 }
--- robot_controller.h
+++ robot_controller.h
@@ -12,11 +12,20 @@ public:
   /// - vx: speed forward [m/s]
   /// - vy: lateral speed [m/s] (+ is left)
   /// - omega: angular speed [rad/s] (+ is rotating to left)
+
   virtual void process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega) = 0;
+  robot_controller ();
 
 private:
-    /// Return the distance from the object with the depth_map.
-    double calc_dist (cv::Mat const & depth_map);
+    /// Return the distance from the object.
+    /// todo
+    float calc_dist (cv::Mat const & left_image, cv::Mat const & right_image);
+
+    float _last_dist;
+    float _last_speed;
+
+    const float INITIAL_SPEED = 4.0;
+    const float DT = 0.5;
 };
 
 #endif // ROBOT_CONTROLLER_H
