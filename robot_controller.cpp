6b9e23f7be6e6311152d2a2e37df02a453ae6de4
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-04-07 15:04:07 +0200

    Ajout du prototype de la methode process + le prototype nul du calcul de distance de mon zbi

---
+++ robot_controller.cpp
@@ -0,0 +1,13 @@
+#include "robot_controller.h"
+
+
+double calc_dist (cv::Mat const & depth_map)
+{
+    // Todo.
+    return 0;
+}
+
+void process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega)
+{
+// Todo.
+}
--- robot_controller.h
+++ robot_controller.h
@@ -3,12 +3,20 @@
 
 #pragma once
 
-#include "controler.h"
+#include "opencv2/core/core.hpp"
 
-class FakeControler : public Controler
+class robot_controller
 {
 public:
-  virtual void process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega) override;
+  /// Use the provided left and right images to update the desired speed
+  /// - vx: speed forward [m/s]
+  /// - vy: lateral speed [m/s] (+ is left)
+  /// - omega: angular speed [rad/s] (+ is rotating to left)
+  virtual void process(const cv::Mat & left_img, const cv::Mat & right_img, float * vx, float * vy, float * omega) = 0;
+
+private:
+    /// Return the distance from the object with the depth_map.
+    double calc_dist (cv::Mat const & depth_map);
 };
 
 #endif // ROBOT_CONTROLLER_H
